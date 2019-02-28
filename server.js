7616e3bea3c75f41d4a90a54d2bbc7e0f77e7fa6
Mason739 <masonraffo@icloud.com>
2019-02-27 19:28:09 -0500

    More stuff

--- server.js
+++ server.js
@@ -13,9 +13,14 @@ router.get('/templates/style/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
+router.get('/background.jpg/',function(req,res){
+  res.sendFile(path.join(__dirname+'/templates/background.jpg'));
+  //__dirname : It will resolve to your project folder.
+});
+
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 8000);
 
-console.log('Running at Port 3000');
+console.log('Running at Port 8000');
