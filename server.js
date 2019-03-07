8ee2dac8fa7015937e738ff551f62c5752781b25
Mason739 <>
2019-03-07 11:50:23 -0500

    Basic file reorganization

--- server.js
+++ server.js
@@ -1,109 +1,109 @@
+// Setup stuff
 const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
 
+
+// Home page
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/templates/index.html'));
-  //__dirname : It will resolve to your project folder.
+
 });
 
+// Browse page
+router.get('/browse/',function(req,res){
+  res.sendFile(path.join(__dirname+'/templates/browse.html'));
+
+});
+
+// Style sheet
 router.get('/templates/style/',function(req,res){
   res.sendFile(path.join(__dirname+'/templates/style.css'));
-  //__dirname : It will resolve to your project folder.
+
 });
 
+
+
+// ============== ALL IMAGES =============
 router.get('/background.jpg/',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/background.jpg'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/background.jpg'));
+
 });
 
 router.get('/strawberry.png/',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/strawberry.png'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/strawberry.png'));
+
 });
 
 router.get('/truck.png/',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/truck.png'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/truck.png'));
+
 });
 
 router.get('/globe.png/',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/globe.png'));
-  //__dirname : It will resolve to your project folder.
-});
+  res.sendFile(path.join(__dirname+'/templates/images/globe.png'));
 
-router.get('/browse/',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/browse.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 router.get('/smoothies/strawberry',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/strawberrySmoothie.jpg'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/strawberrySmoothie.jpg'));
+
 });
 
 router.get('/smoothies/blueberry',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/blueberrySmoothie.jpg'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/blueberrySmoothie.jpg'));
+
 });
 
 router.get('/smoothies/kale',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/kaleSmoothie.jpg'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/kaleSmoothie.jpg'));
+
 });
 
 router.get('/smoothies/pome',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/pomegranateSmoothie.jpg'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/pomegranateSmoothie.jpg'));
+
 });
 
 router.get('/smoothies/banana',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/bananaSmoothie.jpg'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/bananaSmoothie.jpg'));
+
 });
 
 router.get('/smoothies/strawberrybanana',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/strawBananaSmoothie.jpg'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/strawBananaSmoothie.jpg'));
+
 });
 
 router.get('/smoothies/superfruit',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/superfruit.jpg'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/templates/images/superfruit.jpg'));
+
 });
 
 router.get('/smoothies/strawbananapb',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/strawberrybananaPB.jpg'));
-  //__dirname : It will resolve to your project folder.
-});
+  res.sendFile(path.join(__dirname+'/templates/images/strawberrybananaPB.jpg'));
 
-
-router.get('/product',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/product.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-router.get('/product/js',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/productJS.js'));
-  //__dirname : It will resolve to your project folder.
 });
 
 router.get('/icon/bottle',function(req,res){
-  res.sendFile(path.join(__dirname+'/templates/bottle.png'));
-  //__dirname : It will resolve to your project folder.
-});
+  res.sendFile(path.join(__dirname+'/templates/images/bottle.png'));
 
-router.get('/test/output.html',function(req,res){
-  res.sendFile(path.join(__dirname+'/output.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 
+// Javascript for product page
+router.get('/product/js',function(req,res){
+  res.sendFile(path.join(__dirname+'/templates/productJS.js'));
+
+});
 
 
 
-// ============ AUTO CREATED LINES ============
+
+
+
+// ============ ALL PRODUCT PAGES ============
 
 router.get('/product/banana',function(req,res){
       res.sendFile(path.join(__dirname+'/templates/products/banana.html'));
@@ -150,7 +150,7 @@ router.get('/product/blueberry',function(req,res){
 
 
 
-//add the router
+// add the router
 app.use('/', router);
 app.listen(process.env.port || 8000);
 
